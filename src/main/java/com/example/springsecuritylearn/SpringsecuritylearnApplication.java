523ee07cb88c4b73a18fb6e05716f6825b89b73b
Sunny-PC <sunnysul57@gmail.com>
2025-02-24 15:07:55 +0530

    Initialize Spring Boot project with basic configuration and dependencies

---
+++ src/main/java/com/example/springsecuritylearn/SpringsecuritylearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritylearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritylearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecuritylearn/SpringsecuritylearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritylearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
